571d5ea4d5b0055fd7b129599612fd5fe3c03e4c
Gugatronic <124404919+Gugatronic@users.noreply.github.com>
2023-02-24 10:58:56 -0300

    URL do backend atualizada

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -59,8 +59,8 @@ function ReadAll() {
 
   // Realizar requisição para backend obtendo a lista de itens
   async function realizarRequisicao() {
-    const url = "http://localhost:3000/item";
-    const response = await fetch(url);
+    //const url = "http://localhost:3000/item";
+    const url = "https://ocean-jornada-fullstack-fevereiro-23-wjzi.onrender.com;    const response = await fetch(url);
     const data = await response.json();
 
     setItems(data);
